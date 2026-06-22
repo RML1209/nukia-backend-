7b0c5f42785b17d2adb17aed5add5129fdf578b3
Developer <developer@pesa.com>
2026-06-22 19:58:31 +0300

    add new backend url generated from render

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "nukia-backend-me3q.onrender.com",
+    "https://nukia-backend-me3q.onrender.com",
     "www.nukia.online",
     "nukia.online",
 ]
--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
+from django.http import HttpResponse
 from django.urls import path, include
-
+def home(request):
+    return HttpResponse("Backend is working 🚀")
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-
+ path("", home),
     path('api/', include('shops.urls')),
 
 ]
